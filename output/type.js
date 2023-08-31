136e91a2498d38c16d0b608b18743fc0b1d47744
Md.Asif Parvez Sarker <AsifParvezsarker26@gmail.com>
2023-08-31 07:32:13 +0600

    basic type learning

---
+++ output/type.js
@@ -0,0 +1,12 @@
+"use strict";
+let profileName;
+let noDeninedType;
+let users = [10, 20, "sdf"];
+let userData;
+noDeninedType = 10;
+noDeninedType = "asdf";
+// profileName = ["10","4"];
+// profileName = [10,10];
+let userName = "Muhammad Abdur Rahman";
+const mulitply = (a, b) => a * b;
+console.log(`The multiply of 9 and 9 is: ${mulitply(9, 9)}`);
---
+++ src/type.ts
@@ -0,0 +1,18 @@
+let profileName: (string | number)[];
+let noDeninedType;
+let users = [10,20,"sdf"]
+let userData: {
+    name: string,
+    bio?: string,
+    age: number
+}[]
+noDeninedType = 10;
+noDeninedType = "asdf"
+// profileName = ["10","4"];
+// profileName = [10,10];
+let userName = "Muhammad Abdur Rahman";
+
+const mulitply = (a: number, b: number) => a*b;
+console.log(`The multiply of 9 and 9 is: ${mulitply(9,9)}`,)
+
+
